d803e0f3409e2130a792278aa2e9fb7cc681a352
Thomas Strub <thomstrub@gmail.com>
2021-02-22 17:12:23 -0800

    updates dummy data and resolvers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { ApolloServer } = require('apollo-server');
 
-//1
 const typeDefs =`
     type Query {
         info: String!
@@ -14,10 +13,23 @@ const typeDefs =`
     }
     `
 
+//1
+    let links = [{
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for GraphQL'
+    }]
+
 //2
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 }
 
