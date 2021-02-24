dbd8eeac7c3ab0dff34d7d9aa4aed40464a02138
Thomas Strub <thomstrub@gmail.com>
2021-02-23 16:10:53 -0800

    moves schema to separate file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 const { ApolloServer } = require('apollo-server');
 
-const typeDefs =`
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Mutation {
-        post(url: String!, description: String!): Link!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-    `
-
 //1
     let links = [{
         id: 'link-0',
